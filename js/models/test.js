49d43b9973b72bbc3ce91077a53c503d1ffd7a42
pharia-le <pharia.le@gmail.com>
2020-06-23 13:19:42 -0700

    Add f()s to render Test & class f() testDropdownListener & associated addTestOption.

--- js/models/test.js
+++ js/models/test.js
@@ -1,6 +1,52 @@
 class Test {
-    constructor(name,content) {
+    constructor(id,name,content) {
+        this.id = id
         this.name = name
         this.content = content
+    }
+
+    // Event Listener for Select a Test
+    static testDropdownListener() {
+        const testDropdown = document.getElementById("test-dropdown")
+        testDropdown.addEventListener("change", (e) => {
+            const id = parseInt(e.target.value)
+            TestService.fetchTest(id)
+        })
+    }
+
+    // Clear a Test
+    clearTest() {
+        const testContainer = document.getElementById("test-container")
+        testContainer.innerHTML = ''
+    }
+
+    // Add a Test
+    addTest() {
+        const testContainer = document.getElementById("test-container")
+        testContainer.value = this.id
+        testContainer.innerHTML += this.testHTML()
+    }
+
+    // Display a Test
+    renderTest() {
+        this.clearTest()
+        this.addTest()
+    }
+
+    // Create HTML for a Test
+    testHTML() {
+        return `
+            <h3>${this.name}</h3>
+            <p>${this.content}</p>
+            `
+    }
+
+    // Add new Test Option to Test Options Selector
+    addTestOption() {
+        const option = document.createElement("option")
+        option.value = this.id
+        option.innerHTML = this.name
+        const testDropdown = document.getElementById("test-dropdown")
+        testDropdown.add(option)
     }
 }

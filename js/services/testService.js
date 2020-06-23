6cb871193e10a569c40785c43f142710a27776ca
pharia-le <pharia.le@gmail.com>
2020-06-23 13:23:40 -0700

    Add class level fetch methods for tests GET requests.

--- js/services/testService.js
+++ js/services/testService.js
@@ -1,5 +1,50 @@
 class TestService {
-    constructor() {
-        this.baseURL = 'http://localhost:3000/tests'
+
+    static baseURL = 'http://localhost:3000/tests'
+
+    static options = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          }
     }
+    
+    // Class Method to Format Test
+
+    static testData(id,name,content) {
+        id = id,
+        name = name,
+        content = content
+    }
+    
+    static fetchTests() {
+        fetch(this.baseURL,this.options)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(objects) {
+            for (const obj of objects) {
+                const {id,name,content} = obj
+                new Test(id,name,content).addTestOption()
+            }
+        })
+        .catch(function(error) {
+            console.log(error.message)
+        })
+    }
+
+    static fetchTest(id) {
+        fetch(`${this.baseURL}/${id}`,this.options)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            const {id,name,content} = object
+            new Test(id,name,content).renderTest()
+        })
+        .catch(function(error) {
+            console.log(error.message)
+        })
+    }
+
 }

4d6ab10db2d48a78e272bf6b6263cbb7c6755059
pharia-le <pharia.le@gmail.com>
2020-06-24 14:37:41 -0700

    Add class method to POST score to DB.

--- js/services/scoreService.js
+++ js/services/scoreService.js
@@ -1,5 +1,30 @@
 class ScoreService {
-    constructor() {
-        this.baseURL = 'http://localhost:3000/scores'
+    static baseURL = 'http://localhost:3000/scores'
+    
+    static postScore() {
+        let scoreData = {
+            'wpm': parseInt(document.querySelector("#wpm h2").innerHTML),
+            'cpm': parseInt(document.querySelector("#cpm h2").innerHTML),
+            'test_id': document.querySelector("#test-container").value
+        }
+        fetch(this.baseURL, {
+            headers: {
+                'Content-Type': 'application/json'
+                },
+            method: 'POST',
+            body: JSON.stringify(scoreData)
+        })
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(obj) {
+            debugger
+            const {id,wpm,cpm} = obj
+            new Test(id,wpm,cpm).addTestOption()
+        })
+        .catch(function(error) {
+            console.log(error.message)
+        })
     }
 }
+
